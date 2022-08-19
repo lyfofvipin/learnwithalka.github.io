14094c1fd690809a667d951f8e8b594f1a8c0e31
Kumar Vipin Yadav <kumarvipinyadav369@gmail.com>
2022-08-19 17:28:17 +0530

    Updating the data as of Friday 19 August 2022 05:28:17 PM IST

--- deploy_changes.sh
+++ deploy_changes.sh
@@ -1,7 +1,13 @@
+# Update the Static Data Files in main branch
+python find_video_info.py $YT_API_KEY
+# Add and Commit the changes
+git add .; git commit -m "Updating the data as of `date`"
+# Push the changes to main branch
+git push origin main
+
+
 # Make New Branch prod to puch changes
 git checkout -b prod
-# Update the Static Data Files
-python find_video_info.py $YT_API_KEY
 # Deploye the New Changes
 hugo
 # Move all the changes to docs dir for deployment
@@ -11,6 +17,6 @@ git add .
 # Commited the Chages to GH repo
 git commit -m "Updating the data as of `date`"
 # Push the Chages
-git push origin prod
+git push origin prod -f
 # Remove the branch
 git checkout main; git branch -D prod
